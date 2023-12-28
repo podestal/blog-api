fe3bb0d1ed3b775922b3002d757462bde1437f9d
podestal <luisrodriguezpodesta@gmail.com>
2023-12-28 11:12:04 -0500

    routing posts sections bodies

--- blog_api/models.py
+++ blog_api/models.py
@@ -5,21 +5,43 @@ class Topic(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
+
 class Author(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     job_title = models.CharField(max_length=255)
     member_since = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}' 
+
 class Post(models.Model):
+
+    STATUS_IN_PROGRESS = 'P'
+    STATUS_COMPLETED = 'C'
+
+    STATUS_CHOICES = [
+        (STATUS_IN_PROGRESS, "In Progress"),
+        (STATUS_COMPLETED, "Completed"),
+    ]
+
     created_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     title = models.CharField(max_length=255)
     topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_IN_PROGRESS)
     # comment
+
+    def __str__(self):
+        return self.title
 
 class Section(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="sections")
     title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
 
 class Body(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name="bodies")
--- blog_api/urls.py
+++ blog_api/urls.py
@@ -6,8 +6,12 @@ router = routers.DefaultRouter()
 
 router.register('posts', views.PostViewSet)
 router.register('topics', views.TopicViewSet)
-router.register('sections', views.SectionViewSet)
-router.register('bodies', views.BodyViewSet)
 router.register('authors', views.AuthorViewSet)
 
-urlpatterns = router.urls
+sections_router = routers.NestedDefaultRouter(router, 'posts', lookup='posts')
+sections_router.register("sections", views.SectionViewSet, basename='sections')
+
+bodies_router = routers.NestedDefaultRouter(sections_router, 'sections', lookup='sections')
+bodies_router.register("bodies", views.BodyViewSet, basename='bodies')
+
+urlpatterns = router.urls + sections_router.urls + bodies_router.urls
